2e0543ee4dd6a145f70e0c77d2561debf999d3bc
Vineet Saini <94193899+DEVILHACKSWELL@users.noreply.github.com>
2026-03-22 17:34:52 +0200

    Import PyTorch and TensorFlow in notes.py
    Added import statements for PyTorch and TensorFlow.

--- notes.py
+++ notes.py
@@ -1,1 +1,2 @@
-I need to start focusing on the core of or we can say that i need to start to focus in the basics of the Natural Lanaguage Processing as i have not been doing well in any field  
+import pytorch
+import tensorflow
